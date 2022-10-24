dedfabfb58ce5efb3fe08096459733f28faa728e
Reyhan Muhammad Januar <reyhanmuhammadj@gmail.com>
2022-10-24 22:52:02 +0700

    add navlink and sidebar

---
+++ components/atom/NavLink/index.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import {
+  HiOutlineHashtag,
+  HiOutlineHome,
+  HiOutlineMenu,
+  HiOutlinePhotograph,
+  HiOutlineUserGroup,
+} from 'react-icons/hi';
+interface NavlinkProps {
+  handleClick?: () => void;
+}
+export default function NavLink({ handleClick }: NavlinkProps) {
+  const links = [
+    { name: 'Discover', to: '/', icon: HiOutlineHome },
+    { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
+    { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
+    { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+  ];
+  return (
+    <div className="mt-10">
+      {links.map((item) => (
+        <Link
+          key={item.name}
+          href={item.to}
+          onClick={() => handleClick && handleClick()}
+        >
+          <a className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400">
+            <item.icon className="w-6 h-6 mr-2" />
+            {item.name}
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+}
--- pages/discover.tsx
+++ pages/discover.tsx
@@ -2,11 +2,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import Error from '../components/atom/Error';
 import Loader from '../components/atom/Error/Loader';
 import SongCard from '../components/molecules/SongCard';
-import { genres } from '../dummy/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 import { RootState } from '../redux/store';
 import { SongTypes } from '../types';
 export default function Discover() {
+  const genres = [
+    { title: 'Pop', value: 'POP' },
+    { title: 'Hip-Hop', value: 'HIP_HOP_RAP' },
+    { title: 'Dance', value: 'DANCE' },
+    { title: 'Electronic', value: 'ELECTRONIC' },
+    { title: 'Soul', value: 'SOUL_RNB' },
+    { title: 'Alternative', value: 'ALTERNATIVE' },
+    { title: 'Rock', value: 'ROCK' },
+    { title: 'Latin', value: 'LATIN' },
+    { title: 'Film', value: 'FILM_TV' },
+    { title: 'Country', value: 'COUNTRY' },
+    { title: 'Worldwide', value: 'WORLDWIDE' },
+    { title: 'Reggae', value: 'REGGAE_DANCE_HALL' },
+    { title: 'House', value: 'HOUSE' },
+    { title: 'K-Pop', value: 'K_POP' },
+  ];
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector(
     (state: RootState) => state.player
